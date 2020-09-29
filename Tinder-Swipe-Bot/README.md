5bdcd870047fe06e64db8acb4e892ad91aab1281
satyendra dhaka <satyendradhaka5@gmail.com>
2020-09-29 10:02:32 +0530

    test

--- Tinder-Swipe-Bot/README.md
+++
@@ -1,7 +0,0 @@
-# Tinder-Bot
-Auto Swiping bot for tinder
-
-
-### Requirements
-1. Selenium
-2. Chrome Web Driver which needs to be installed seperately
--- Tinder-Swipe-Bot/secrets.py
+++
@@ -1,2 +0,0 @@
-username=input("enter username of facebook: ") #update it with email registered with facebook
-password=input('enter password: ')  #enter your password here
